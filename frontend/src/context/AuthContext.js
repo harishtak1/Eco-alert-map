7e06d5640f28f32b292a87188a49c2271f10f29f
harishtak1 <harishsain891@gmail.com>
2025-09-16 22:14:55 +0530

    Add files via upload

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,24 @@
+import { createContext, useState, useContext } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+    setUser(userData); // { username, email, role }
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// ✅ custom hook
+export const useAuth = () => useContext(AuthContext);
